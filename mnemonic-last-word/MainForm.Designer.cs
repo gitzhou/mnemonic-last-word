b9679d625256361af2d28409af2b009b6a02cb93
aaron67 <aaron67@aaron67.cc>
2019-02-26 23:25:19 +0800

    Initial commit

---
+++ mnemonic-last-word/MainForm.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace mnemonic_last_word
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MnemonicLabel = new System.Windows.Forms.Label();
+            this.MnemonicTextBox = new System.Windows.Forms.TextBox();
+            this.LastWordTextBox = new System.Windows.Forms.TextBox();
+            this.LastWordLabel = new System.Windows.Forms.Label();
+            this.CalculateButton = new System.Windows.Forms.Button();
+            this.HintLabel = new System.Windows.Forms.Label();
+            this.ClearButton = new System.Windows.Forms.Button();
+            this.LastPrefixTextBox = new System.Windows.Forms.TextBox();
+            this.LastPrefixLabel = new System.Windows.Forms.Label();
+            this.ChecksumTextBox = new System.Windows.Forms.TextBox();
+            this.ChecksumLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // MnemonicLabel
+            // 
+            this.MnemonicLabel.AutoSize = true;
+            this.MnemonicLabel.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.MnemonicLabel.Location = new System.Drawing.Point(25, 24);
+            this.MnemonicLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.MnemonicLabel.Name = "MnemonicLabel";
+            this.MnemonicLabel.Size = new System.Drawing.Size(306, 36);
+            this.MnemonicLabel.TabIndex = 6;
+            this.MnemonicLabel.Text = "Mnemonic except last";
+            // 
+            // MnemonicTextBox
+            // 
+            this.MnemonicTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.MnemonicTextBox.Location = new System.Drawing.Point(340, 24);
+            this.MnemonicTextBox.Multiline = true;
+            this.MnemonicTextBox.Name = "MnemonicTextBox";
+            this.MnemonicTextBox.Size = new System.Drawing.Size(492, 187);
+            this.MnemonicTextBox.TabIndex = 1;
+            // 
+            // LastWordTextBox
+            // 
+            this.LastWordTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.LastWordTextBox.Location = new System.Drawing.Point(340, 367);
+            this.LastWordTextBox.Name = "LastWordTextBox";
+            this.LastWordTextBox.ReadOnly = true;
+            this.LastWordTextBox.Size = new System.Drawing.Size(492, 35);
+            this.LastWordTextBox.TabIndex = 6;
+            // 
+            // LastWordLabel
+            // 
+            this.LastWordLabel.AutoSize = true;
+            this.LastWordLabel.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.LastWordLabel.Location = new System.Drawing.Point(187, 367);
+            this.LastWordLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.LastWordLabel.Name = "LastWordLabel";
+            this.LastWordLabel.Size = new System.Drawing.Size(144, 36);
+            this.LastWordLabel.TabIndex = 8;
+            this.LastWordLabel.Text = "Last word";
+            // 
+            // CalculateButton
+            // 
+            this.CalculateButton.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.CalculateButton.Location = new System.Drawing.Point(269, 461);
+            this.CalculateButton.Name = "CalculateButton";
+            this.CalculateButton.Size = new System.Drawing.Size(150, 50);
+            this.CalculateButton.TabIndex = 3;
+            this.CalculateButton.Text = "Calculate";
+            this.CalculateButton.UseVisualStyleBackColor = true;
+            this.CalculateButton.Click += new System.EventHandler(this.CalculateButton_Click);
+            // 
+            // HintLabel
+            // 
+            this.HintLabel.AutoSize = true;
+            this.HintLabel.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.HintLabel.Location = new System.Drawing.Point(37, 67);
+            this.HintLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.HintLabel.Name = "HintLabel";
+            this.HintLabel.Size = new System.Drawing.Size(294, 36);
+            this.HintLabel.TabIndex = 10;
+            this.HintLabel.Text = "(Seperated by space)";
+            // 
+            // ClearButton
+            // 
+            this.ClearButton.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.ClearButton.Location = new System.Drawing.Point(437, 461);
+            this.ClearButton.Name = "ClearButton";
+            this.ClearButton.Size = new System.Drawing.Size(150, 50);
+            this.ClearButton.TabIndex = 4;
+            this.ClearButton.Text = "Clear";
+            this.ClearButton.UseVisualStyleBackColor = true;
+            this.ClearButton.Click += new System.EventHandler(this.ClearButton_Click);
+            // 
+            // LastPrefixTextBox
+            // 
+            this.LastPrefixTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.LastPrefixTextBox.Location = new System.Drawing.Point(340, 239);
+            this.LastPrefixTextBox.Name = "LastPrefixTextBox";
+            this.LastPrefixTextBox.Size = new System.Drawing.Size(492, 35);
+            this.LastPrefixTextBox.TabIndex = 2;
+            this.LastPrefixTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.LastPrefixTextBox_KeyPress);
+            // 
+            // LastPrefixLabel
+            // 
+            this.LastPrefixLabel.AutoSize = true;
+            this.LastPrefixLabel.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.LastPrefixLabel.Location = new System.Drawing.Point(12, 239);
+            this.LastPrefixLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.LastPrefixLabel.Name = "LastPrefixLabel";
+            this.LastPrefixLabel.Size = new System.Drawing.Size(319, 36);
+            this.LastPrefixLabel.TabIndex = 13;
+            this.LastPrefixLabel.Text = "Last word binary prefix";
+            // 
+            // ChecksumTextBox
+            // 
+            this.ChecksumTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ChecksumTextBox.Location = new System.Drawing.Point(340, 303);
+            this.ChecksumTextBox.Name = "ChecksumTextBox";
+            this.ChecksumTextBox.ReadOnly = true;
+            this.ChecksumTextBox.Size = new System.Drawing.Size(492, 35);
+            this.ChecksumTextBox.TabIndex = 5;
+            // 
+            // ChecksumLabel
+            // 
+            this.ChecksumLabel.AutoSize = true;
+            this.ChecksumLabel.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.ChecksumLabel.Location = new System.Drawing.Point(179, 303);
+            this.ChecksumLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.ChecksumLabel.Name = "ChecksumLabel";
+            this.ChecksumLabel.Size = new System.Drawing.Size(152, 36);
+            this.ChecksumLabel.TabIndex = 15;
+            this.ChecksumLabel.Text = "Checksum";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(844, 529);
+            this.Controls.Add(this.ChecksumTextBox);
+            this.Controls.Add(this.ChecksumLabel);
+            this.Controls.Add(this.LastPrefixTextBox);
+            this.Controls.Add(this.LastPrefixLabel);
+            this.Controls.Add(this.ClearButton);
+            this.Controls.Add(this.HintLabel);
+            this.Controls.Add(this.CalculateButton);
+            this.Controls.Add(this.LastWordTextBox);
+            this.Controls.Add(this.LastWordLabel);
+            this.Controls.Add(this.MnemonicTextBox);
+            this.Controls.Add(this.MnemonicLabel);
+            this.MinimumSize = new System.Drawing.Size(870, 600);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Mnemonic Last Word";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label MnemonicLabel;
+        private System.Windows.Forms.TextBox MnemonicTextBox;
+        private System.Windows.Forms.TextBox LastWordTextBox;
+        private System.Windows.Forms.Label LastWordLabel;
+        private System.Windows.Forms.Button CalculateButton;
+        private System.Windows.Forms.Label HintLabel;
+        private System.Windows.Forms.Button ClearButton;
+        private System.Windows.Forms.TextBox LastPrefixTextBox;
+        private System.Windows.Forms.Label LastPrefixLabel;
+        private System.Windows.Forms.TextBox ChecksumTextBox;
+        private System.Windows.Forms.Label ChecksumLabel;
+    }
+}
+
---
+++ mnemonic-last-word/MainForm.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+
+namespace mnemonic_last_word
+{
+    public partial class MainForm : Form
+    {
+        private static readonly int TOTAL_WORD_COUNT_IN_LIST = 2048;
+        private static readonly int[] VALID_MNEMONIC_WORD_COUNT = { 12, 15, 18, 21, 24 };
+        private static readonly int BIT_LENGTH_PER_WORD = 11;
+
+        private List<string> english = new List<string>();
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, System.EventArgs e)
+        {
+            LoadMnemonic();
+        }
+
+        public void LoadMnemonic()
+        {
+            using (StreamReader sr = new StreamReader(@"wordlist\english.txt"))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    english.Add(line);
+                }
+            }
+            if (english.Count != TOTAL_WORD_COUNT_IN_LIST)
+            {
+                MessageBox.Show("Mnemonic word list file is broken, please reset the file", "Load Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Application.Exit();
+            }
+        }
+
+        private void CalculateButton_Click(object sender, System.EventArgs e)
+        {
+            try
+            {
+                // Choose a language
+                List<string> wordList = english;
+
+                // Validation of word list
+                string[] words = MnemonicTextBox.Text.Trim().Split(' ');
+                string checkMessage = IsWordsValid(words, wordList);
+                if (checkMessage != "")
+                {
+                    MessageBox.Show(checkMessage, "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                // Validation of the last word prefix
+                string lastWordPrefix = LastPrefixTextBox.Text.Trim();
+                int validPrefixLength = GetValidPrefixLength(words.Length);
+                if (lastWordPrefix.Length != validPrefixLength)
+                {
+                    MessageBox.Show("The length of last word prefix should be " + validPrefixLength, "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                // We're good here
+
+                // Calculate word index
+                List<int> wordIndex = new List<int>();
+                foreach (string word in words)
+                {
+                    wordIndex.Add(wordList.IndexOf(word));
+                }
+                // Calculate cresponding binary stream of word index
+                StringBuilder builder = new StringBuilder();
+                foreach (int index in wordIndex)
+                {
+
+                    builder.Append(Convert.ToString(index, 2).PadLeft(BIT_LENGTH_PER_WORD, '0'));
+                }
+                // Got the raw entropy
+                builder.Append(lastWordPrefix);
+                string entropy = builder.ToString();
+                // Calculate SHA256 of raw entropy
+                byte[] hash = new SHA256Managed().ComputeHash(BinaryStringToBytes(entropy));
+                // Got the checksum of mnemonic
+                int checksumLength = BIT_LENGTH_PER_WORD - validPrefixLength;
+                string checksum = BytesToBinaryString(hash).Substring(0, checksumLength);
+                ChecksumTextBox.Text = checksum;
+                // Got the index of last word, append prefix with checksum
+                int lastWordIndex = Convert.ToInt32(lastWordPrefix + checksum, 2);
+                // Locate last word in list
+                LastWordTextBox.Text = wordList[lastWordIndex];
+            }
+            catch
+            {
+                MessageBox.Show("Unknown exception", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private string IsWordsValid(string[] words, List<string> wordList)
+        {
+            // Validation of word count
+            if (Array.IndexOf(VALID_MNEMONIC_WORD_COUNT, words.Length + 1) < 0)
+            {
+                return "The word count is not correct";
+            }
+            // Each word should be in the list
+            foreach (string word in words)
+            {
+                if (wordList.IndexOf(word) < 0)
+                {
+                    return "Word " + word + " is invalid";
+                }
+            }
+            // Word list is good here
+            return "";
+        }
+
+        // According to BIP39
+        // the last word consist of two parts
+        // some low bits of raw entropy and some high bits of checksum
+        // We can figure out the checksum length from the mnemonic word count
+        // then decide the length of last word prefix since each word has 11 bits
+        //
+        //  Word Count | Checksum Length | Prefix Length
+        // ----------- | --------------- | ---------------
+        //     12      |        4        |        7
+        //     15      |        5        |        6
+        //     18      |        6        |        5
+        //     21      |        7        |        4
+        //     24      |        8        |        3
+        private int GetValidPrefixLength(int wordCount)
+        {
+            return BIT_LENGTH_PER_WORD - (wordCount + 1) / 3;
+        }
+
+        private void ClearButton_Click(object sender, System.EventArgs e)
+        {
+            MnemonicTextBox.Text = "";
+            LastPrefixTextBox.Text = "";
+            ChecksumTextBox.Text = "";
+            LastWordTextBox.Text = "";
+        }
+
+        private void LastPrefixTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Only accept 0, 1, backspace and delete
+            if (e.KeyChar == '0' || e.KeyChar == '1' || (Keys)(e.KeyChar) == Keys.Back || (Keys)(e.KeyChar) == Keys.Delete)
+            {
+                e.Handled = false;
+                return;
+            }
+            e.Handled = true;
+        }
+
+        #region Conversion methods of byte array and string of binary format
+
+        private static byte[] BinaryStringToBytes(string binString)
+        {
+            int BITS_OF_BYTE = 8;
+            binString = binString.Replace(" ", "").Replace("\t", "").Replace("\r", "").Replace("\n", "");
+            while ((binString.Length % BITS_OF_BYTE) != 0)
+            {
+                binString = "0" + binString;
+            }
+            byte[] bytes = new byte[binString.Length / BITS_OF_BYTE];
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                bytes[i] = Convert.ToByte(binString.Substring(i * BITS_OF_BYTE, BITS_OF_BYTE), 2);
+            }
+            return bytes;
+        }
+
+        private static string BytesToBinaryString(byte[] bytes)
+        {
+            StringBuilder builder = new StringBuilder();
+            foreach (byte b in bytes)
+            {
+                builder.Append(Convert.ToString(b, 2).PadLeft(8, '0'));
+            }
+            return builder.ToString();
+        }
+
+        #endregion
+    }
+}
